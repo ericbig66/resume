175979e8ee3240b7f61c9dcef1fb6ffab75e8ca9
ericbig66 <47971342+ericbig66@users.noreply.github.com>
2019-05-23 02:02:18 +0800

    image blur added
    add image blur when hover most of the photos
    delete the unused files to minimize the usage of space

---
+++ js/pic_control.js
@@ -0,0 +1,6 @@
+$(window).load(function() {
+    $('.works').find('img').each(function() {
+        var imgClass = (this.width / this.height > 1) ? 'wide' : 'tall';
+        $(this).addClass(imgClass);
+    })
+})
